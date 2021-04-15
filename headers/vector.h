03531ec72eac4d9349216100885e404fca203a94
tim-tim707 <timothee.denizou@epita.fr>
2021-04-15 22:59:58 +0200

    Cpp vector implementation (automatic size ajust)

---
+++ headers/vector.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdlib.h>
+#include <assert.h>
+#include <err.h>
+
+#define DEFAULTCAPACITY 16
+
+typedef struct Vector
+{
+    size_t capacity;
+    size_t length;
+    void **data; // array of struct pointers
+}Vector;
+
+Vector *newVector();
+void freeVector(Vector *v); // free the vector without freeing its content
+
+void vectorPush(Vector *v, void *dataPtr); // add an element at last index
+void *vectorPop(Vector *v); // remove element at last index. usage : (myStruct *)vectorPop(v);
+
+void *vectorGet(Vector *v, size_t i); // usage : (myStruct *)vectorGet(v, i);
+
+void vectorInsert(Vector *v, size_t i, void *dataPtr); // insert and moves next elements to the right
+void *vectorRemove(Vector *v, size_t i); // return the struct at indice i and move next elements to the left
+
+int vectorIsEmpty(Vector *v);
+
---
+++ src/vector.c
@@ -0,0 +1,90 @@
+#include "vector.h"
+
+Vector *newVector()
+{
+    Vector *v = malloc(sizeof(Vector));
+    v->capacity = DEFAULTCAPACITY;
+    v->length = 0;
+    v->data = calloc(v->capacity, sizeof(void *));
+
+    return v;
+}
+
+void freeVector(Vector *v)
+{
+    free(v->data);
+    free(v);
+}
+
+static void doubleCapacity(Vector *v)
+{
+    v->capacity *= 2;
+    v->data = realloc(v->data, v->capacity * sizeof(void *));
+}
+
+static void reduceCapacity(Vector *v)
+{
+    if(v->capacity > DEFAULTCAPACITY)
+    {
+        v->capacity /= 2;
+        v->data = realloc(v->data, v->capacity * sizeof(void *));
+    }
+}
+
+void vectorPush(Vector *v, void *dataPtr)
+{
+    v->data[v->length] = dataPtr;
+    v->length++;
+
+    if(v->length == v->capacity)
+        doubleCapacity(v);
+}
+
+void *vectorPop(Vector *v)
+{
+    void *result = v->data[v->length - 1];
+    v->length--;
+
+    if(v->length < v->capacity / 4)
+        reduceCapacity(v);
+
+    return result;
+}
+
+void *vectorGet(Vector *v, size_t i)
+{
+    assert(i < v->length);
+    return v->data[i];
+}
+
+void vectorInsert(Vector *v, size_t i, void *dataPtr)
+{
+    assert(i < v->length);
+    v->length++;
+
+    if(v->length == v->capacity)
+        doubleCapacity(v);
+
+    for(size_t j = v->length; j > i; j--)
+        v->data[j] = v->data[j - 1];
+    v->data[i] = dataPtr;
+}
+
+void *vectorRemove(Vector *v, size_t i)
+{
+    void *result = v->data[i];
+
+    for(size_t j = i; j < v->length; j++)
+        v->data[j] = v->data[j + 1];
+
+    v->length--;
+    if(v->length < v->capacity / 4)
+        reduceCapacity(v);
+
+    return result;
+}
+
+int vectorIsEmpty(Vector *v)
+{
+    return v->length == 0;
+}
